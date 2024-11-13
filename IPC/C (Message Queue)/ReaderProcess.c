0d85b380d8cc2b10d322609fa2482bc1f3ced50e
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-11-13 06:32:28 -0400

    Translate comments in ReaderProcess.c and add URL reference in WriterProcess.c

--- IPC/C (Message Queue)/ReaderProcess.c
+++ IPC/C (Message Queue)/ReaderProcess.c
@@ -1,9 +1,9 @@
-// C Program for Message Queue (Reader Process)
+// Programa en C para Cola de Mensajes (Proceso Lector)
 #include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
-// structure for message queue
+// estructura para la cola de mensajes
 struct mesg_buffer {
     long mesg_type;
     char mesg_text[100];
@@ -14,22 +14,27 @@ int main()
     key_t key;
     int msgid;
 
-    // ftok to generate unique key
+    // ftok para generar una clave única
     key = ftok("progfile", 65);
 
-    // msgget creates a message queue
-    // and returns identifier
+    // msgget crea una cola de mensajes
+    // y devuelve el identificador
     msgid = msgget(key, 0666 | IPC_CREAT);
 
-    // msgrcv to receive message
+    // msgrcv para recibir el mensaje
+    // el tercer parámetro es el tamaño del mensaje
+    // el cuarto parámetro es el tipo de mensaje que queremos recibir
+    // el quinto parámetro son las opciones de recepción (0 para esperar hasta que llegue un mensaje)
     msgrcv(msgid, &message, sizeof(message), 1, 0);
 
-    // display the message
-    printf("Data Received is : %s \n", 
+    // mostrar el mensaje recibido
+    printf("Datos recibidos: %s \n", 
                     message.mesg_text);
 
-    // to destroy the message queue
+    // destruir la cola de mensajes
     msgctl(msgid, IPC_RMID, NULL);
 
     return 0;
 }
+
+//https://www.geeksforgeeks.org/ipc-using-message-queues/
--- IPC/C (Message Queue)/WriterProcess.c
+++ IPC/C (Message Queue)/WriterProcess.c
@@ -35,3 +35,6 @@ int main()
 
     return 0;
 }
+
+
+//https://www.geeksforgeeks.org/ipc-using-message-queues/
